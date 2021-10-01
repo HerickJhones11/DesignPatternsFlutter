403d6df57867bcd58ed2ce3d58cac04524d91930
HerickJhones11 <pauloherick@gmail.com>
2021-10-01 11:59:05 -0300

    add loginpage layout

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,36 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:fordev/ui/components/components.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(children: [
-          Container(
-            child: Image(image: AssetImage('lib/ui/assets/logo.png'),),
-          ),
-          Text('Login'.toUpperCase()),
-          Form(child: Column(children: [
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-                icon: Icon(Icons.email)
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+          LoginHeader(),
+          HeadLine1(text: 'Login',),
+          Padding(
+            padding: const EdgeInsets.all(32),
+            child: Form(child: Column(children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  icon: Icon(Icons.email, color: Theme.of(context).primaryColorLight,)
+                ),
+                keyboardType: TextInputType.emailAddress,
               ),
-              keyboardType: TextInputType.emailAddress,
+              Padding(
+                padding: const EdgeInsets.only(top: 8,bottom: 32),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    icon: Icon(Icons.email, color: Theme.of(context).primaryColorLight,)
+                  ),
+                  obscureText: true,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {}, 
+                child: Text('Entrar'.toUpperCase()),
+                ),
+              TextButton.icon(onPressed: () {}, icon: Icon(Icons.person), label: Text('Criar Conta'))
+            ],)
             ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-                icon: Icon(Icons.email)
-              ),
-              obscureText: true,
-            ),
-            ElevatedButton(
-              onPressed: () {}, 
-              child: Text('Entrar'.toUpperCase()),
-              ),
-            TextButton.icon(onPressed: () {}, icon: Icon(Icons.person), label: Text('Criar Conta'))
-          ],)
           )
         ],),
       ),
